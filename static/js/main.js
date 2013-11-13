6fb3e793bfd764234f2ff0b97b75600c788ddd4f
Samuel Okoroafor <samuel.i.okoroafor@gmail.com>
2013-11-13 07:32:52 +0100

    Stabilized project. Ready for API binding

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,258 @@
 /**
- * @author tomiwa
- */'use strict';
+ * Created by LRB on 10/28/13.
+ */
 
+var pollplusModule = angular.module("PollPlus", []);
+var baseURL = "http://localhost:16000/api";
+var contestants = [];
+var polls = [{
+	"id" : 1,
+	"title" : "Cloud Hackathon",
+	"description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	"type" : "public",
+	"status" : "published",
+	"ownerID" : "098876654453678",
+	"voteCount" : 76,
+	"contestants" : [{
+		"name" : "Kola Jamay",
+		"information" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		"photoURL" : "/static/img/python.png",
+		"code" : "4"
+	}, {
+		"name" : "Ijaware Tom",
+		"information" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		"photoURL" : "/static/img/python.png",
+		"code" : "5"
+	}, {
+		"name" : "Tobay Tom",
+		"information" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		"photoURL" : "/static/img/python.png",
+		"code" : "5"
+	}]
+}, {
+	"id" : 2,
+	"title" : "MTN GApps challenge",
+	"description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	"type" : "public",
+	"status" : "published",
+	"ownerID" : "098876654453678",
+	"voteCount":86,
+	"contestants" : [{
+		"name" : "Okoro Tugbaski",
+		"information" : "Sharp guy",
+		"photoURL" : "/static/img/python.png",
+		"code" : "4"
+	}, {
+		"name" : "Babanriga",
+		"information" : "Badoo",
+		"photoURL" : "/static/img/python.png",
+		"code" : "5"
+	}]
+}];
 
-angular.module('pollific', [])
-.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.when('/', {
-            templateUrl : 'static/tmpl/index.html',
-            controller : 'MainCtrl'
-        }).when('/polls', {
-            templateUrl : 'static/tmpl/polls.html',
-            controller : 'PollsCtrl'
-        }).otherwise({
-            redirectTo : '/'
-        });
-    }])
-.factory('polls', ['$http', function($http){
-    var baseUrl = "/api/polls";
-    return {
-        get : function(poll_id) {
-            return $http.get(baseUrl + '/' + poll_id);
-        },
-        save : function(poll) {
-            var url = poll.id ? baseUrl + '/' + poll_id : baseUrl;
-            return $http.post(url, poll);
-        },
-        query : function() {
-            return $http.get(baseUrl + '/');
-        }
-    };
-}]);
+var pTypes = ['private', 'public'];
+pollplusModule.config(function($httpProvider) {
+	$httpProvider.defaults.useXDomain = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
 
-var MainCtrl = function($scope, polls) {
-    $scope.polls = polls.query();
-    $scope.greetings = "Hello World, You are the last dragon";
-};
+//Configure the route config
+function routeConfig($routeProvider) {
+	//The first should be like a timeline of trending Polls
+	$routeProvider.when('/', {
+		controller : PollListController,
+		templateUrl : '/static/tmpl/listUserPolls.html'
+	}).
+	//next is for creating polls
+	when('/createPoll', {
+		controller : CreatePollController,
+		templateUrl : '/static/tmpl/createPoll.html'
+	}).//next is for getting poll statistics of a poll
+	when('/poll/:id', {
+		controller : PollStatsController,
+		templateUrl : '/static/tmpl/pollStats.html'
+	}).
+	//this controls the polls a user voted in
+	when('/pollsVoted',{
+		controller: PollsVotedController,
+		templateUrl: '/static/tmpl/pollsVoted.html'
+	}).
+	//Viewing the polls created by a user
+	when('/createdPolls',{
+		controller: CreatedPollsController,
+		templateUrl: '/static/tmpl/createdPolls.html'
+	}).
+	//this is for viewing a particular poll
+	when('/view/:id', {
+		controller : PollDetailController,
+		templateUrl : '/static/tmpl/pollDetail.html'
+	}).otherwise({
+		redirectTo : '/'
+	});
 
-var PollsCtrl = function($scope) {
-    $scope.greetings = "Hello World, You are now in Pollific";
-};
+}
+
+//wire up the config
+pollplusModule.config(routeConfig);
+
+//create the pollservice
+pollplusModule.factory('pollService', function($http) {
+	//access the pollservice and fetch data
+	return {
+		getPolls : function() {//mock poll objects
+			return polls;
+			/* var url = baseURL + "/polls/";
+			 $http.get(url).success(function (data) {
+			 //.log(data);
+			 return data;
+			 }).error(function (data) {
+			 console.log("Error");
+			 //console.log(data);
+			 });*/
+
+		},
+		//send the json object from the poll
+		postPoll : function(data) {
+			/*var url = baseURL + "/polls/";
+			 $http.post(url, data).success(function (result) {
+			 console.log("Got this data for the poll ID: " + result);
+			 return result;
+			 }).error(function (result) {
+			 console.log("Error: "+result);
+			 });*/
+			polls.splice(1, 0, data);
+			console.log(polls);
+			return true;
+
+		},
+		postContestants : function(pollid, data) {
+			/*var url = baseURL + "/polls/"+pollid+"/contestants/";
+			 $http.post(url, data).success(function (response) {
+			 console.log(response);
+			 alert("Poll Successfully created");
+			 //window.location="/home";
+			 })
+			 .error(function (response) {
+			 console.log(response);
+			 });*/
+
+		},
+		vote : function() {
+			// body...
+
+		},
+		getUserId : function() {
+			var url = '/gateway';
+			$http.get(url).success(function(data) {
+				console.log("SUCCESS: " + data);
+				return data;
+
+			}).error(function(data) {
+				console.log("ERROR: " + data);
+			})
+		}
+	}
+});
+//the poll list Controller
+function PollListController($scope, pollService) {
+	//pass in the service to get the list of mock polls
+	//console.log(pollService.getUserId());
+	$scope.polls = pollService.getPolls();
+}
+
+function CreatePollController($scope, pollService) {
+	//{"desc": "This is the content of my first poll", "title": "First Poll", "id": 5275456790069248}
+	//Poll details
+	//Call service to get Poll ID
+
+	//var userId = pollService.getUserId();
+	$scope.polls = pollService.getPolls();
+	$scope.contestants = contestants;
+	$scope.pollTypes = pTypes;
+	$scope.addContestant = function() {
+		$scope.contestants.splice(1, 0, {
+			name : $scope.fullname,
+			info : $scope.info,
+			image : "nothing for now",
+			id : ""
+		});
+		$scope.fullname = "";
+		$scope.info = "";
+	};
+	$scope.createPoll = function() {
+
+		/* var object = {user: userId, title: title, desc: pollInfo};
+		 var pollID = pollService.postPoll(object);
+		 //Use the retrieved pollID to post contestants
+		 for(var i=0;i<$scope.contestants.length;i++)
+		 {
+		 $scope.contestants[i].id=pollID;
+		 }
+		 var response = pollService.postContestants(pollID,$scope.contestants);
+		 console.log("Final Post result: " + response);*/
+
+		var conts = $scope.contestants;
+		var title = $scope.title;
+		var pollInfo = $scope.pollInfo;
+		var poll = {};
+		poll.title = title;
+		poll.information = pollInfo;
+		poll.contestants = conts;
+		//var result = pollService.postPoll(poll);
+		$scope.polls.splice(1, 0, poll);
+		console.log($scope.polls);
+		$scope.result = "";
+		$scope.result = 'Poll created successfully';
+		//$scope.createResult.class="alert alert-success";
+		$scope.clear();
+
+	};
+	$scope.remove = function(index) {
+		$scope.contestants.splice(index, 1);
+
+	}
+	$scope.clear = function() {
+		$scope.title = "";
+		$scope.pollInfo = "";
+		$scope.contestants = [];
+	}
+}
+
+function PollDetailController($scope, $routeParams, pollService) {
+	//a model to control the display text on  the button
+	$scope.action="Vote";
+	var pollcount = pollService.getPolls();
+	var pollTemp = {};
+	for (var i = 0; i < pollcount.length; i++) {
+		if (pollcount[i].id == $routeParams.id) {
+			pollTemp = pollcount[i];
+		}
+		$scope.poll = pollTemp;
+	}
+	$scope.vote = function() {
+		var result = confirm("Are you sure?");
+		if (result == true) {
+			alert("Vote casted!");
+		}
+	}
+}
+
+function PollStatsController($scope, $routeParams, pollService) {
+	//Get the Poll statistics
+	var pollss = pollService.getPolls();
+	var pollStat = {};
+	for (var i = 0; i < pollss.length; i++) {
+		if (pollss[i].id == $routeParams.id) {
+			pollStat = pollss[i];
+		}
+		$scope.pollStatistic = pollStat;
+	}
+}
+
+function PollsVotedController($scope,pollService) {
+$scope.polls=pollService.getPolls();
+}
+function CreatedPollsController($scope,pollService)
+{
+	$scope.polls=pollService.getPolls();
+}
