d3cff02a0c256e5e254c2e27295c9f6a3fd077ff
adekola adebayo <adekola.m.adebayo@gmail.com>
2013-11-13 04:03:01 +0100

    implemented version 1 of the poll-plus API...to meet the Minimum Viable Product Requirements of the App - Polls, Contestants and Votes

--- routes.py
+++ routes.py
@@ -16,11 +16,23 @@ _routes = [
     ]),
     PathPrefixRoute('/api', [
         RedirectRoute('/', handler = 'handlers.index.ApiHandler', name = 'api_uri'),
+        #GET and POST allowed to this URL - for a get, a specified number of results should be specified to
+        # paginate the results returned...
+        #GETs a list of polls giving you just id title and description
+        #or
+        #POSTs a new poll to the server by supplying details of the poll as well as the contestants if applicable
         RedirectRoute('/polls/', handler = 'handlers.poll_handlers.PollsHandler', name = 'polls_uri'),
-        RedirectRoute('/polls/<id:\d+>', handler = 'handlers.poll_handlers.PollHandler', name = 'poll_uri'),
+        #GETs the full details of a particular poll...details such as ownerID, status, etc
+        RedirectRoute('/polls/<poll_id:\d+>', handler = 'handlers.poll_handlers.PollHandler', name = 'poll_uri'),
+        #GETs the contestants for a particular poll
         RedirectRoute('/polls/<poll_id:\d+>/contestants', handler = 'handlers.poll_handlers.ContestantsHandler', name = 'poll_contestants_uri'),
-        RedirectRoute('/contestant', handler = 'handlers.poll_handlers.ContestantsHandler', name = 'contestants_uri'),
-        RedirectRoute('/vote', handler = 'handlers.poll_handlers.VoteHandler', name = 'vote_uri')
+        #POSTs a vote to a particular poll
+        RedirectRoute('/polls/<poll_id:\d+>/vote', handler = 'handlers.poll_handlers.VoteHandler', name = 'vote_uri'),
+        #GETs the votes for a particular poll
+        RedirectRoute('/polls/<poll_id:\d+>/votes', handler = 'handlers.poll_handlers.VotesHandler', name = 'vote_uri'),
+        #GETs the details of a particular contestant
+        RedirectRoute('/contestants/<contestant_id:\d+>', handler = 'handlers.poll_handlers.ContestantHandler', name = 'contestants_uri'),
+
     ]),
     PathPrefixRoute('/u',[
         RedirectRoute('/login', handler = 'handlers.user.LoginUserHandler', name = 'login_uri', strict_slash = True),
